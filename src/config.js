9930673cee82819b2266113aec36fcfc15782d17
danielroga <daniel.rodriguez.gambetta@hotmail.com>
2020-08-11 22:05:34 -0400

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "dans-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ps8zg66r9d.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Ng5CNobGJ",
+      APP_CLIENT_ID: "1td6ali6m7i2llnaiae0779svr",
+      IDENTITY_POOL_ID: "us-east-1:67e94cf6-c8da-443f-bbb8-f890bb135414"
+    }
+  };
